3750dfd5ca7242f66f2e1ad032e0756e145340b9
Anastasia <hello.aplic@gmail.com>
2024-11-27 12:05:53 +0300

    [TrackersViewController] NavigationBar is configured

--- Tracker/Controllers/TrackersViewController.swift
+++ Tracker/Controllers/TrackersViewController.swift
@@ -9,8 +9,62 @@ import UIKit
 
 final class TrackersViewController: UIViewController {
     
+    private lazy var addTrackerButton: UIButton = {
+        let button = UIButton.systemButton(
+            with: UIImage(named: "AddButton")!,
+            target: self,
+            action: #selector(self.didTapAddTrackerButton)
+        )
+        button.tintColor = .trackerBlack
+        return button
+    }()
+    
+    private lazy var datePickerButton: UIBarButtonItem = {
+        let datePicker = UIDatePicker()
+        datePicker.locale = Locale(identifier: "ru_RU")
+        datePicker.preferredDatePickerStyle = .compact
+        datePicker.datePickerMode = .date
+        datePicker.addTarget(self, action: #selector(self.didTapDatePicker), for: .valueChanged)
+        let rightBarButtonItem = UIBarButtonItem(customView: datePicker)
+        return rightBarButtonItem
+    }()
+    
+    private lazy var searchBar: UISearchController = {
+        let search = UISearchController(searchResultsController: nil)
+        search.searchBar.placeholder = "Поиск"
+        search.hidesNavigationBarDuringPresentation = false
+        return search
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupNavigationBar()
+    }
+    
+    private func setupNavigationBar() {
+        navigationItem.title = "Трекеры"
+        navigationController?.navigationBar.prefersLargeTitles = true
         
+        navigationItem.leftBarButtonItem = UIBarButtonItem(customView: addTrackerButton)
+        
+        datePickerButton.customView?.widthAnchor.constraint(equalToConstant: 100).isActive = true
+        datePickerButton.customView?.heightAnchor.constraint(equalToConstant: 34).isActive = true
+        navigationItem.rightBarButtonItem = datePickerButton
+        
+        navigationItem.searchController = searchBar
+    }
+    
+    @objc
+    private func didTapAddTrackerButton() {
+        
+    }
+    
+    @objc
+    private func didTapDatePicker(_ sender: UIDatePicker) {
+        let selectedDate = sender.date
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd.MM.yyyy"
+        let formattedDate = dateFormatter.string(from: selectedDate)
+        print("Выбранная дата: \(formattedDate)")
     }
 }
--- Tracker/Main/MainTabBarController.swift
+++ Tracker/Main/MainTabBarController.swift
@@ -11,24 +11,30 @@ final class MainTabBarController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.tabBar.backgroundColor = .trackerWhite
+        setupTabBar()
         createTabBarItem()
     }
     
     private func createTabBarItem() {
-        let trackersTabBarItem = TrackersViewController()
+        let trackersTabBarItem = UINavigationController(rootViewController: TrackersViewController())
         trackersTabBarItem.tabBarItem = UITabBarItem(
             title: "Трекеры",
             image: UIImage(named: "trackers.svg"),
             selectedImage: nil
         )
         
-        let statisticsTabBarItem = StatisticsViewController()
+        let statisticsTabBarItem = UINavigationController(rootViewController: StatisticsViewController())
         statisticsTabBarItem.tabBarItem = UITabBarItem(
             title: "Статистика",
             image: UIImage(named: "stats.svg"),
             selectedImage: nil
         )
         self.viewControllers = [trackersTabBarItem, statisticsTabBarItem]
+    }
+    
+    private func setupTabBar() {
+        let line = UIView(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: 1))
+        line.backgroundColor = UIColor.black.withAlphaComponent(0.3)
+        self.tabBar.addSubview(line)
     }
 }
